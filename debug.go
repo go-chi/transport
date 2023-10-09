9e28425789924d015e5ab40655ed496525f644d8
Vojtech Vitek <vojtech.vitek@golang.cz>
2023-10-09 19:45:36 +0200

    Simplify Debug middleware

--- debug.go
+++ debug.go
@@ -8,19 +8,17 @@ import (
 	"moul.io/http2curl/v2"
 )
 
-func Debug() func(http.RoundTripper) http.RoundTripper {
-	return func(next http.RoundTripper) http.RoundTripper {
-		return RoundTripFunc(func(req *http.Request) (resp *http.Response, err error) {
-			curlCommand, _ := http2curl.GetCurlCommand(req)
-			log.Printf("%v", curlCommand)
-			log.Printf("request: %s %s", req.Method, req.URL)
+func Debug(next http.RoundTripper) http.RoundTripper {
+	return RoundTripFunc(func(req *http.Request) (resp *http.Response, err error) {
+		curlCommand, _ := http2curl.GetCurlCommand(req)
+		log.Printf("%v", curlCommand)
+		log.Printf("request: %s %s", req.Method, req.URL)
 
-			startTime := time.Now()
-			defer func() {
-				log.Printf("response (%v): %v %s", time.Since(startTime), resp.Status, resp.Request.URL)
-			}()
+		startTime := time.Now()
+		defer func() {
+			log.Printf("response (%v): %v %s", time.Since(startTime), resp.Status, resp.Request.URL)
+		}()
 
-			return next.RoundTrip(req)
-		})
-	}
+		return next.RoundTrip(req)
+	})
 }
